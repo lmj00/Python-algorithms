a0202ed7b9161ad66824e3fb030f722e70d53c69
이민진 <e.minjin00@gmail.com>
2021-08-23 22:38:40 +0900

    Feat: Brute Force related find_nearby_store

---
+++ Brute Force/find_nearby_store.py
@@ -0,0 +1,27 @@
+#제곱근 사용을 위한 sqrt 함수
+from math import sqrt
+
+#두 매장의 직선 거리를 계산해 주는 함수
+def distance(store1, store2):
+    return sqrt((store1[0] - store2[0]) ** 2 + (store1[1] - store2[1]) ** 2)
+
+#가장 가까운 두 매장을 찾아주는 함수
+def closest_pair(coordinates):
+    # 여기 코드를 쓰세요
+    value = []
+    dist = distance(coordinates[0], coordinates[1])
+    
+    for i in range(len(coordinates) -1):
+        for j in range(i + 1, len(coordinates)): 
+            if dist > distance(coordinates[i], coordinates[j]): 
+                dist = distance(coordinates[i], coordinates[j])
+                value = [coordinates[i], coordinates[j]]
+    return value
+ 
+ 
+                
+#테스트
+test_coordinates = [(2, 3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)]
+print(closest_pair(test_coordinates))
+ 
+                                  
